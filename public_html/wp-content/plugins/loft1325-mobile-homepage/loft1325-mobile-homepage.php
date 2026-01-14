49fb141ef406496fd6355bf9048923e6b9908a5e
waroxa <waroxa2@gmail.com>
2026-01-14 09:03:45 -0800

    Allow booking search pages on mobile

--- public_html/wp-content/plugins/loft1325-mobile-homepage/loft1325-mobile-homepage.php
+++ public_html/wp-content/plugins/loft1325-mobile-homepage/loft1325-mobile-homepage.php
@@ -168,6 +168,16 @@ if ( ! class_exists( 'Loft1325_Mobile_Homepage' ) ) {
                 return false;
             }
 
+            $search_page_id = (int) get_option( 'nd_booking_search_page' );
+
+            if ( $search_page_id && is_page( $search_page_id ) ) {
+                return false;
+            }
+
+            if ( is_post_type_archive( 'nd_booking_cpt_1' ) ) {
+                return false;
+            }
+
             if ( isset( $_GET['loft1325_mobile_preview'] ) && '1' === $_GET['loft1325_mobile_preview'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
                 return true;
             }
@@ -419,9 +429,11 @@ if ( ! class_exists( 'Loft1325_Mobile_Homepage' ) ) {
             }
 
             $target = '';
+            $search_page_id = 0;
 
 			if ( function_exists( 'nd_booking_search_page' ) ) {
 				$target = nd_booking_search_page();
+				$search_page_id = (int) get_option( 'nd_booking_search_page' );
 			}
 
 			if ( ! $target ) {
@@ -431,6 +443,14 @@ if ( ! class_exists( 'Loft1325_Mobile_Homepage' ) ) {
 			if ( ! $target ) {
 				return;
 			}
+
+            if ( $search_page_id && is_page( $search_page_id ) ) {
+                return;
+            }
+
+            if ( is_post_type_archive( 'nd_booking_cpt_1' ) ) {
+                return;
+            }
 
 			$language = $this->get_current_language();
 
@@ -449,9 +469,14 @@ if ( ! class_exists( 'Loft1325_Mobile_Homepage' ) ) {
 
             $query_args = array_filter(
                 $query_args,
-                static function ( $value ) {
+                static function ( $value, $key ) {
+                    if ( 'nd_booking_archive_form_guests' === $key ) {
+                        return true;
+                    }
+
                     return '' !== $value && null !== $value;
-                }
+                },
+                ARRAY_FILTER_USE_BOTH
             );
 
             $query_string = http_build_query( $query_args );
